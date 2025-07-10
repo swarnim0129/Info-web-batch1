af8d1181dc6577677bc0b9737177a875f99db2f0
Swarnim Bane <136164833+swarnim0129@users.noreply.github.com>
2025-07-10 18:27:58 +0530

    Lec3 Python

--- Python basics/lec-3.py
+++ Python basics/lec-3.py
@@ -49,16 +49,136 @@
 #     print(i)
 #     i+=2
 
-passwrod="admin123"
-attempts = 0
+#-----------------------------------------------------------------------------------------
+# passwrod="admin123"
+# attempts = 0
 
-while attempts < 3:
-    entered_password = input("Enter your password: ")
-    if entered_password == passwrod:
-        print("Access granted")
-        break
-    else:
-        attempts += 1
-        print(f"Incorrect password. You have {3 - attempts} attempts left.")
-if attempts == 3:
-    print("Access denied. Too many incorrect attempts.")
+# while attempts < 3:
+#     entered_password = input("Enter your password: ")
+#     if entered_password == passwrod:
+#         print("Access granted")
+#         break
+#     else:
+#         attempts += 1
+#         print(f"Incorrect password. You have {3 - attempts} attempts left.")
+# if attempts == 3:
+#     print("Access denied. Too many incorrect attempts.")
+
+#-----------------------------------------------------------------------------------------------
+# fruits=["banana","bpple","cherry"]
+
+# print(fruits[0])
+
+# for f in fruits:
+#     print(f)
+
+# fruits.append("litchie")
+# print(fruits)
+
+# # fruits.remove("banana")
+# # print(fruits)
+
+# fruits.insert(3,"orange")
+# print(fruits)
+
+# fruits.pop()
+# print(fruits)
+
+# fruits.sort(reverse=True)
+# print(fruits)
+
+# nums=[10,2,3,5,6]
+# nums.sort(reverse=True)
+# print(nums)
+
+# fruits.clear()
+# print(fruits)
+
+#-----------------------------------------------------
+
+#dictionaries
+
+# student={
+#     'name':"Rahul",
+#     'age':20,
+#     'city':"Mumbai",
+# }
+
+# # print(type(student))
+# # for key in student:
+# #     print(key , student[key])
+
+# # for key , value in student.items():
+# #     print(f"{key} => {value}")
+
+# student["email"]="xyz@gmail.com"
+# print(student)
+
+# student.update({'phone':123456})
+# print(student)
+
+# student.update({'name':"Ram"})
+# print(student)
+
+
+# student.pop('age')
+# print(student)
+
+# print(student.keys())
+# print(student.values())
+
+
+#----------------------------------------------------
+#Tuples
+
+# a=(1,2,3,4,5,5,3,5,5)
+# # print(a[2])
+
+# b=(1)
+# print(type(b))
+
+# # for i in a:
+# #     print(i)
+
+# print(a.count(5))
+# print(a.index(5))
+
+# print(a)
+
+# a[0]=100
+# print(a)
+
+#------------------------------------------------------
+# Sets
+
+colors={"red","yellow","purple",0.1,0.2,3,4,5,6,7,8,"green"}
+floats={0.1,0.2,0.3,0.4}
+print(colors)
+
+
+# colors.add("yellow")
+# print(colors)
+
+# colors.remove("green")
+# print(colors.remove("green"))
+
+# colors.discard("yellow")
+# # print(colors.discard("yellow"))
+
+# # print(colors[0])
+# colors.pop()
+
+
+
+# lists=[]
+# tuples=()
+# sets={}
+# dictionaries={'key':'value'}
+
+print(colors.union(floats))
+print(colors.intersection(floats))
+
+# print(colors.difference(floats))
+a={1,2,3}
+b={3,4,5}
+print(b.difference(a))
