40992861327bf8f9fb2059cc4348a51b724c7536
jaimeenbhagat <jaimeenbhagat@outlook.com>
2025-07-10 15:55:23 +0400

    Lec-3

---
+++ Python basics/lec-3.py
@@ -0,0 +1,50 @@
+#For loop While loop
+#range(start, stop-1, step)
+#1 2 3 4 5 6 7 8 9 10
+# for i in range(2,11,1):
+#     print(i)
+
+# name = "Python"
+# for i in name:
+#     print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits)   
+# for i in fruits:
+#     print(i)
+
+# numbers = [1, 2, 3, 4, 5]
+# for i in numbers:
+#     print(i+3)
+
+# for i in range(11,1,-1):
+#     print(i)
+
+# #While loop
+# i=10
+# while i > 1:
+#     print(i)
+#     i -= 1
+
+# name = "Python"
+# i = 0
+# while i < len(name):
+#     print(name[i])
+#     i += 1
+
+# fruits = ["apple", "banana", "cherry"]
+# i=0
+# while i < len(fruits):
+#     print(fruits[i])
+#     i += 1
+
+# numbers = [1, 2, 3, 4, 5]
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i] + 3)
+#     i += 1
+
+# i=1
+# while i <=10:
+#     print(i)
+#     i+=2
